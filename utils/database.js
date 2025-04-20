35fbc1f3b05ebca91eefae064cb3c711274f04f8
kacygd <164707129+kacygd@users.noreply.github.com>
2025-04-20 14:28:27 +0700

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,13 @@
-import { Database } from "bun:sqlite";
+import Database from 'better-sqlite3';
 import { join } from "path";
 import { readFileSync } from "node:fs";
 
-const db = new Database(join(__dirname, "..", "data", "database.db"), { create: true });
+const db = new Database(join(__dirname, "..", "data", "database.db"), { fileMustExist: false });
 try {
-	db.exec(readFileSync(join(__dirname, "..", "data", "database.sql"), "utf8"));
-	db.exec("PRAGMA journal_mode = WAL;");
-} catch(e) {
-	console.error(e);
+    db.exec(readFileSync(join(__dirname, "..", "data", "database.sql"), "utf8"));
+    db.exec("PRAGMA journal_mode = WAL;");
+} catch (e) {
+    console.error(e);
 }
 
 export { db };
